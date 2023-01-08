8696197fcf9aa4ea3b2c3920bb67fc5231577d08
marioveljanov <marioveljanov1@gmail.com>
2023-01-08 18:55:12 +0100

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+      <header className="header">
+        <img
+          src={
+            "https://camo.githubusercontent.com/bf47be85ae361ed72b63c8d2dc415df4bffb3d8a3dd2ba381da01302784bedaf/68747470733a2f2f63646e2e737667706f726e2e636f6d2f6c6f676f732f62756e6e792d6e65742d69636f6e2e737667"
+          }
+        />
+      </header>
+    );
+}
+
+
+export default Header;
